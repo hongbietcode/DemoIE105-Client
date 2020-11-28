c6c628d9253a6a2c01b6f2041e1991e7d44806fe
shin <huutrimega@gmail.com>
2020-11-28 14:59:16 +0700

    public

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-// export const BASE_SERVER_URL = "http://demo-antt-nhom1.herokuapp.com";
-export const BASE_SERVER_URL = "http://localhost";
+export const BASE_SERVER_URL = "http://demo-antt-nhom1.herokuapp.com";
+// export const BASE_SERVER_URL = "http://localhost";
 export const DIFFIE_HELLMAN_GROUP = "modp1";
